33b97d07fa63eee914f9b3f89fa5868b629ffcf1
Sonu Kumar <sksonu8544@gmail.com>
2024-02-11 19:08:44 +0530

    fix home image

--- src/components/main.jsx
+++ src/components/main.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import './main.css'
 
 const Home = () => {
   return (
@@ -7,16 +8,16 @@ const Home = () => {
         <div className="card bg-dark text-white border-0 mx-3">
           <img
             className="card-img img-fluid"
-            src="./assets/main.png.jpg"
+            src="./assets/main.jpg"
             alt="Card"
             height={500}
           />
           <div className="card-img-overlay d-flex align-items-center">
-            <div className="container">
-              <h5 className="card-title fs-1 text fw-lighter">New Season Arrivals</h5>
-              <p className="card-text fs-5 d-none d-sm-block ">
+            <div className="container" id="mainContainer">
+              <h5 className="card-title fs-1 ">New Season Arrivals</h5>
+              <p className="card-text fs-5 d-none d-sm-block">
                 This is a wider card with supporting text below as a natural
-                lead-in to additional content. This content is a little bit
+                lead-in <br /> to additional content. This content is a little bit
                 longer.
               </p>
             </div>
